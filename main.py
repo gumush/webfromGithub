abb808f20ea2af8cb86628cd2c52037b96b4c796
Hikmet Gumus <gumush@gmail.com>
2021-12-04 21:10:47 +0300

    first files

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI ,Request
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse, FileResponse
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+app.mount("/static/", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+async def show_test(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/home")
+async def show_test(request: Request):
+ return FileResponse('static/index.html', media_type='text/html')
